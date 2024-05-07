d590cc43b6318fb4325962ad3a7cff30b28f4a14
fronterscc <fronterscc@noreply.codeberg.org>
2024-05-07 14:09:07 +0000

    Update static/scripts/loadingRing.js

--- static/scripts/loadingRing.js
+++ static/scripts/loadingRing.js
@@ -1,9 +1,10 @@
 //loading ring
-window.onload = function () {
-    document.getElementById('root').style.visibility='hidden';
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
 };
-
-function bodyLoaded() {
-    document.getElementById('loader').style.visibility='hidden';
-    document.querySelector('root').style.visibility='visible';
-}
